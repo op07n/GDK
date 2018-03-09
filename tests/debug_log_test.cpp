519e2620ba05bbb5770e503c9aa746c7d278055b
Joseph Cameron <jf.cameron@gmail.com>
2018-03-09 17:10:20 -0500

    wip debug test

--- tests/debug_log_test.cpp
+++ tests/debug_log_test.cpp
@@ -12,56 +12,65 @@ static constexpr auto TAG = "DebugLogTest";
 TEST_START
 { "Log", [&]()
 {
-    auto *const pclogrdbuf = std::clog.rdbuf();    
+    auto *const pRawDeviceStringBuffer = std::clog.rdbuf();    
     
     std::stringstream sstream;
     std::clog.rdbuf(sstream.rdbuf());
 
     Debug::log(TAG, "Hi", 123, 1.2f, false);
     
-    std::clog.rdbuf(pclogrdbuf);
+    std::clog.rdbuf(pRawDeviceStringBuffer);
 
     if (sstream.str() == "D/DebugLogTest: Hi1231.20\n") test_succeeded();
 }},
 
 { "Error", [&]()
 {
-    auto *const pclogrdbuf = std::cerr.rdbuf();    
+    auto *const pRawDeviceStringBuffer = std::cerr.rdbuf();    
     
     std::stringstream sstream;
     std::cerr.rdbuf(sstream.rdbuf());
 
     Debug::error(TAG, "Thin" "k", " hard: ", 0.266f, false);
     
-    std::cerr.rdbuf(pclogrdbuf);
+    std::cerr.rdbuf(pRawDeviceStringBuffer);
 
     if (sstream.str() == "E/DebugLogTest: Think hard: 0.2660\n") test_succeeded();
 }},
 
-{ "User-defined logger", [&]()
+{ "User-defined logger, custom behaviour", [&]()
 {
     auto logger = Debug::Logger([](const std::string &aMessage)
     {
         std::cout << "Custom/" << aMessage << std::endl;
     });
 
-    auto *const pclogrdbuf = std::cout.rdbuf();    
+    auto *const pRawDeviceStringBuffer = std::cout.rdbuf();    
     
     std::stringstream sstream;
     std::cout.rdbuf(sstream.rdbuf());
 
     logger.log("qwerty");
     
-    std::cout.rdbuf(pclogrdbuf);
+    std::cout.rdbuf(pRawDeviceStringBuffer);
 
     if (sstream.str() == "Custom/qwerty\n") test_succeeded();
 }},
 
-/*
-{ "Example", [&]()
+{ "User-defined logger, default behaviour", [&]()
 {
+    Debug::Logger logger;
 
+    auto *const pRawDeviceStringBuffer = std::clog.rdbuf();    
+    
+    std::stringstream sstream;
+    std::clog.rdbuf(sstream.rdbuf());
+
+    logger.log("qwerty");
+    
+    std::clog.rdbuf(pRawDeviceStringBuffer);
+
+    if (sstream.str() == "qwerty\n") test_succeeded();
 }},
-*/
 
 TEST_END
